7abc7e47624dba1ed32a4e3e600aa81745663293
AsnelChristian <ngoullasob@gmail.com>
2020-12-21 22:42:47 +0100

    Init project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hey clever programmer let's build this clone</h1>
+    </div>
+  );
+}
+
+export default App;
